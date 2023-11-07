84f0e71a83e56ad4ec694f40931ef4230a03d495
Ian Mugambi <linksian63@gmail.com>
2023-11-07 07:47:55 +0300

    chore: update configure

--- startup.bat
+++ startup.bat
@@ -2,4 +2,4 @@ docker rm "portfolio.online" -f
 docker rmi "portfolio.online" -f
 
 COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 docker-compose build
-docker run -d  -p 1338:1338 --name "portfolio.online" --network portfolio-network -i -t "portfolio.online" 
+docker run -d --restart unless-stopped -p 172.18.0.1:1338:1338 --name "portfolio.online" --network portfolio-network -i -t "portfolio.online" 
